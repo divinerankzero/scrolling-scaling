513504d01a972d24e3bdabb837fb5ccfc6c95c44
Keith Potempa <keithrpotempa@gmail.com>
2020-01-22 12:28:46 -0600

    configured the min values using if else statements

--- audrey.js
+++ audrey.js
@@ -1,28 +1,19 @@
 const audrey = document.getElementById("audrey")
 
-/*
-    [x] Add an event listener to the `document` object to listen
-    for the "scroll" event.
-*/
-
 const adjustAudrey = (event) => {
-    audrey.style.width = `${window.scrollY / 3}px`;
-    audrey.style.height = `${window.scrollY / 4}px`;
+    if (window.scrollY / 3 > 50){
+        audrey.style.width = `${window.scrollY / 3}px`;
+    } else {
+        audrey.style.widows = "50px";
+    }
+    if (window.scrollY / 4 > 100) {
+        audrey.style.height = `${window.scrollY / 4}px`;
+    } else {
+        audrey.style.height = "100px";
+    }
+    
 } 
 
 document.addEventListener("scroll", adjustAudrey)
-    /*
-        [x] Adjust the width of audrey to be 1/3 the value of
-        `window.scrollY`. 
-        [ ] No lower than 50px, though.
-    */
-
-
-
-    /*
-        [x] Adjust the height of audrey to be 1/4 the value of
-        `window.scrollY`. 
-        [ ] No lower than 100px, though.
-    */
 
     
