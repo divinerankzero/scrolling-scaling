d8da453d55655ad86da8edeed2772b60746fb630
Keith Potempa <keithrpotempa@gmail.com>
2020-01-22 12:24:13 -0600

    finished exercise except the minimum values

--- audrey.js
+++ audrey.js
@@ -1,19 +1,28 @@
 const audrey = document.getElementById("audrey")
 
 /*
-    Add an event listener to the `document` object to listen
+    [x] Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-.addEventListener("", function () {
+
+const adjustAudrey = (event) => {
+    audrey.style.width = `${window.scrollY / 3}px`;
+    audrey.style.height = `${window.scrollY / 4}px`;
+} 
+
+document.addEventListener("scroll", adjustAudrey)
     /*
-        Adjust the width of audrey to be 1/3 the value of
-        `window.scrollY`. No lower than 50px, though.
+        [x] Adjust the width of audrey to be 1/3 the value of
+        `window.scrollY`. 
+        [ ] No lower than 50px, though.
     */
 
 
 
     /*
-        Adjust the height of audrey to be 1/4 the value of
-        `window.scrollY`. No lower than 100px, though.
+        [x] Adjust the height of audrey to be 1/4 the value of
+        `window.scrollY`. 
+        [ ] No lower than 100px, though.
     */
-})
+
+    
